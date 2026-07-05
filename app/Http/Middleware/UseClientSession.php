70f83b623191e257d9866db89aa435cb6de3bbd9
Forkan <forkanalam295@gmail.com>
2026-07-06 04:29:35 +0600

    single session for client and admin conflict resolve

---
+++ app/Http/Middleware/UseClientSession.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class UseClientSession
+{
+    public function handle(Request $request, Closure $next)
+    {
+        config(['session.cookie' => 'client-session']);
+
+        return $next($request);
+    }
+}
